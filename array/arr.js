f4247d8d407d6c3a83c4147b8422a2b75c15bb46
Kumarahul06 <kumarahul1321@gmail.com>
2023-08-16 13:34:19 +0530

    array

---
+++ array/arr.js
@@ -0,0 +1,19 @@
+var vehicle = ["Bus","Train","Bike","Car","villa","Banglo","Aeroplane",];
+
+console.log(vehicle[0]);
+console.log(vehicle[1]);
+console.log(vehicle[2]);
+console.log(vehicle[3]);
+console.log(vehicle[4]);
+
+
+//slice
+console.log(vehicle.slice(2));
+console.log(vehicle.slice(0,2));
+console.log(vehicle.slice(1,5));
+
+
+//splice
+console.log(vehicle.splice(2, 2));
+console.log(vehicle);
+
